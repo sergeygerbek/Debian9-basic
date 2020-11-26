acc77554ac1c67ca9e9d9b7e7d3e5ba16e15b180
sergeygerbek <sergey.gerbek@gmail.com>
2020-11-26 13:44:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,3 +17,19 @@ mkdir -p /root/.ssh && echo "$SUPUBKEY" >> /root/.ssh/authorized_keys
 mkdir -p /home/$SUUSER/.ssh && echo "$SUPUBKEY" >> /home/$SUUSER/.ssh/authorized_keys
 chmod -R 700 /root/.ssh
 chmod -R 700 /home/${SUUSER}/.ssh && chown -R ${SUUSER}:${SUUSER} /home/${SUUSER}/.ssh
+
+#DISABLE login over SSH with password and for root
+sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/" /etc/ssh/sshd_config
+systemctl restart sshd
+
+#SETUP fail2ban
+apt-get -o Acquire::ForceIPv4=true install -y fail2ban
+cd /etc/fail2ban && cp fail2ban.conf fail2ban.local && cp jail.conf jail.local
+systemctl enable fail2ban
+systemctl start fail2ban
+
+
+(sleep 5; reboot) &
